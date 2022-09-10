2ac1da22233747956809815466bb6c8b49c0fa01
rahman-habib <97686740+rahman-habib@users.noreply.github.com>
2022-09-10 04:03:32 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,157 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected, sendEther } from './interaction';
+import { ethers } from "ethers";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  const [transferAddress, setTransferAddress] = useState("");
+  const [transferAmount, setTransferAmount] = useState("");
+  const [transferStatus, setTransferStatus] = useState("");
+
+  useEffect(() => {
+
+    async function setup() {
+      const { address, status } = await getCurrentWalletConnected();
+
+      setWallet(address);
+
+      setStatus(status);
+
+      addWalletListener();
+
+    }
+    setup();
+  }, []);
+
+  const getBalance = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    const ethbalance = await provider.getBalance(walletAddress)
+    const balanceInEth = ethers.utils.formatEther(ethbalance)
+
+
+    setStatus(balanceInEth);
+  };
+
+  const onTransferBalancePressed = async () => {
+    const { status } = await sendEther(walletAddress, transferAddress, transferAmount);
+    setTransferStatus(status);
+  };
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Populate the Data and Click on Button to execute...");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {" "}
+          🦊{" "}
+          <a target="_blank" rel="noreferrer" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+  return (
+    <>
+    <div className="container">
+
+      <button className="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+      
+      <div>
+        <ul>
+          <li>
+          <h2 style={{ paddingTop: "5px" }}>Transfer Balance</h2>
+          </li>
+          <li>
+            
+          </li>
+          <li>        <input
+            type="text"
+            placeholder="Enter Wallet address 0x..."
+            onChange={(e) => setTransferAddress(e.target.value)}
+            value={transferAddress}
+          />
+          </li>
+          <li>        <input
+            type="text"
+            placeholder="Enter Amount to be transferred"
+            onChange={(e) => setTransferAmount(e.target.value)}
+            value={transferAmount}
+          />
+          </li>
+         
+          <li>
+            <button className="publish" onClick={onTransferBalancePressed}>
+              Transfer Balance
+            </button>
+          </li>
+          
+          <li>
+            <button className="balance" onClick={getBalance}>
+              Balance
+            </button>
+          
+          </li>
+
+        
+        </ul>
+        </div>
+       
+
+
+
+
+
+
+
+
+      
+
+
     </div>
+    <div className='show'>
+       <div>
+        <ul>
+        <li>
+            <p id="status">{transferStatus}</p>
+          </li>
+          <li>
+          <p id="status">{status}</p>
+          </li>
+          </ul>
+      </div>
+    </div>
+    </>
   );
 }
 
---
+++ src/interaction.js
@@ -0,0 +1,105 @@
+export const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const obj = {
+          
+          address: addressArray[0],
+        };
+        return obj;
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" rel="noreferrer" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
+  export const sendEther=async( sender,receiver,val)=>{
+    const transactionParameters = {
+        to: receiver, // Required except during contract publications.
+        from: sender, // must match user's active address.
+        value: val, // Only required to send ether to the recipient from the initiating external account.
+       };
+      
+      // txHash is a hex string
+      
+      // As with any RPC call, it may throw an error
+      try {
+        const txHash = await window.ethereum.request({
+          method: 'eth_sendTransaction',
+          params: [transactionParameters],
+        });
+        return {
+         
+          status: txHash,
+        };
+      } catch (error) {
+        return {
+         
+          status: error.message,
+        };
+      }
+      
+  }
+
+  
+  export const getCurrentWalletConnected = async () => {
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (addressArray.length > 0) {
+          return {
+            address: addressArray[0],
+            
+          };
+        } else {
+          return {
+            address: "",
+            status: "🦊 Connect to Metamask using the top button.",
+          };
+        }
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" rel="noreferrer" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
